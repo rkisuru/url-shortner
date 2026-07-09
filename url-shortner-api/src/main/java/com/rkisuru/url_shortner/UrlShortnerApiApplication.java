b5afa173e539e6476e91d74648a4db263fdf9ca6
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2026-07-09 15:27:52 +0530

    init project

---
+++ url-shortner-api/src/main/java/com/rkisuru/url_shortner/UrlShortnerApiApplication.java
@@ -0,0 +1,13 @@
+package com.rkisuru.url_shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerApiApplication.class, args);
+	}
+
+}
---
+++ url-shortner-api/src/test/java/com/rkisuru/url_shortner/UrlShortnerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkisuru.url_shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
