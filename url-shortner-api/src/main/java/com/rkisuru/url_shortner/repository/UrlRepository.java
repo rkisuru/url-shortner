cc2ce371385bed794a76e55912e6d6bf317e7d87
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2026-07-13 16:13:03 +0530

    handled the problem of concurrent click count increment using postgres feature

--- url-shortner-api/src/main/java/com/rkisuru/url_shortner/repository/UrlRepository.java
+++ url-shortner-api/src/main/java/com/rkisuru/url_shortner/repository/UrlRepository.java
@@ -2,6 +2,9 @@ package com.rkisuru.url_shortner.repository;
 
 import com.rkisuru.url_shortner.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,4 +12,8 @@ public interface UrlRepository extends JpaRepository<Url, Long> {
 
     Optional<Url> findByShortCode(String shortCode);
     boolean existsByShortCode(String shortCode);
+
+    @Modifying
+    @Query("UPDATE Url u SET u.clickCount = u.clickCount + 1 WHERE u.shortCode = :shortCode")
+    void incrementClickCount(@Param("shortCode") String shortCode);
 }
