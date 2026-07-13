95b2e4e224afca155046955e54747a84256d3d11
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2026-07-13 16:13:24 +0530

    create url controller with endpoints

---
+++ url-shortner-api/src/main/java/com/rkisuru/url_shortner/controller/UrlController.java
@@ -0,0 +1,32 @@
+package com.rkisuru.url_shortner.controller;
+
+import com.rkisuru.url_shortner.dto.CreateUrlRequest;
+import com.rkisuru.url_shortner.dto.UrlResponse;
+import com.rkisuru.url_shortner.services.UrlService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/urls")
+@RequiredArgsConstructor
+public class UrlController {
+
+    private final UrlService urlService;
+
+    @PostMapping
+    public ResponseEntity<UrlResponse> createShortUrl(@Valid @RequestBody CreateUrlRequest request) {
+        UrlResponse response = urlService.createShortUrl(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortCode) {
+        String longUrl = urlService.resolveLongUrl(shortCode);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header("Location", longUrl)
+                .build();
+    }
+}
